19ee91f61e0d01ac876008257a5724b1c2c0162b
João Victor <joaovictorpinheiro2007@gmail.com>
2025-01-24 22:46:20 -0300

    Add empty directories with .gitkeep

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
+#include <openssl/sha.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    
+    SHA_CTX *ctx;
+    SHA1_Init(ctx);
+
+    const void *d;
+    size_t len;
+    SHA1_Update(ctx, d, len);
+
+    unsigned char *md_buf
+    SHA1_Final(md_buf, ctx);
+
+    unsigned char *data;
+    size_t count;
+    SHA1(data, count, md_buf);
+
     return 0;
 }
